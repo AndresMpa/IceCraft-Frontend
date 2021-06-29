465cc1350ca79d09ff1243178745e731f70cc5c2
AndresMpa <andresprieto2703@gmail.com>
2021-06-28 19:28:55 -0500

    Ajustes para poder visualizar

--- src/main.js
+++ src/main.js
@@ -8,22 +8,22 @@ import vuetify from "./plugins/vuetify";
 Vue.config.productionTip = false;
 const base = axios.create({
   //Esto sale de heroku
-  baseURL: "https://blooming-fjord-22200.herokuapp.com/api"
+  //baseURL: "https://blooming-fjord-22200.herokuapp.com/api"
 
 
   //Esta URL solo será usado mientras corremos las pruebas
   //es necesario hacer deploy a heroku
-  //baseURL: "http://localhost:3000/api"
+  baseURL: "http://localhost:3000/api"
 
 })
 
 Vue.prototype.$http = base;
 //Esto sale de heroku
-//axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL = "http://localhost:3000/api";
 
 //Esta URL solo será usado mientras corremos las pruebas
 //es necesario hacer deploy a heroku
-axios.defaults.baseURL = "https://blooming-fjord-22200.herokuapp.com/api";
+//axios.defaults.baseURL = "https://blooming-fjord-22200.herokuapp.com/api";
 
 new Vue({
   router,
